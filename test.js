c1a5a44c65b55d924652f08f3e2faff354e73f06
Owen <zeelyn@qq.com>
2022-12-11 17:06:08 +0800

    dev

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 import http from "./lib/http.js";
 http.defaults((config) => {
-    config.baseURL = "https://localhost:44313";
+    config.baseURL = "https://www.google.com";
+    config.timeout = 0;
     config.$on_before_request = (options) => {
         //options.headers["Authorization"] = "Bearer " + localStorage.getItem("access_token");
     };
@@ -13,21 +14,21 @@ http.defaults((config) => {
     config.$error_network = (err) => {
         console.error("network error ", err);
     };
-    // config.proxy = {
-    //     protocol: "http",
-    //     host: "localhost",
-    //     port: 8888,
-    //     auth: {
-    //         username: "mikeymike",
-    //         password: "rapunz3l",
-    //     },
-    // };
+    config.proxy = {
+        protocol: "http",
+        host: "thecore.222233.xyz",
+        port: 29102,
+        auth: {
+            username: "43595",
+            password: "fQruf8dNTJ0I",
+        },
+    };
 });
 
-http.get("/api/account/check", {})
+http.get("/translate_a/single?client=webapp&ie=UTF-8&sl=auto&tl=en&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=gt&pc=1&otf=1&ssel=0&tsel=0&kc=1&tk=11", {})
     .then((res) => {
         console.log("ok=>", res.data);
     })
     .catch((err) => {
-        console.error(err);
+        // console.error(err);
     });
